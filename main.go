59c36f7ce0d004c2eebc4ec013beaa2451e54d97
vlad.shuisky208 <vlad.shuisky@gmail.com>
2025-05-05 16:22:47 +0300

    commenting LoadEnv, to start on railway

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"log"
-	"github.com/VladShuisky/vodokanalbot/utils"
+	// "log"
+	// "github.com/VladShuisky/vodokanalbot/utils"
 	"github.com/VladShuisky/vodokanalbot/bot"
 )
 
 
 func main() {
 	// Создаем бота
-	err := utils.LoadEnv(".env")
-	if err != nil {
-		log.Fatal("Error loading .env file:", err)
-	}
+	// err := utils.LoadEnv(".env")
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file:", err)
+	// } //закомментено, чтобы заработало на railway
 	bot.StartBot()
 }
